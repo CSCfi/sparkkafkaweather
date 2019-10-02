34092f1ae9f75556bc40356c75787fea723f2283
juhahu <juha.hulkkonen@gmail.com>
2019-10-02 16:02:34 +0300

    Cleaned code

--- src/main/java/fi/csc/spark/weather/sparkweather/SparkApp.java
+++ src/main/java/fi/csc/spark/weather/sparkweather/SparkApp.java
@@ -1,48 +1,43 @@
 package fi.csc.spark.weather.sparkweather;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.hadoop.security.UserGroupInformation;
+//import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
+//import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
+
 import fi.csc.spark.weather.kafka.KafkaWeatherConsumer;
 
-/**
- * Hello world!
- *
- */
-public class SparkApp 
+public class SparkApp
 {
     public static void main( String[] args ) throws InterruptedException
     {
-       
+    	//Juha
+		UserGroupInformation.setLoginUser(UserGroupInformation.createRemoteUser("spark"));
 
-    	SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("Weather Average");
+    	SparkConf sparkConf = new SparkConf().setMaster("spark://testspark-spark-master:7077").setAppName("Weather Average");
+    	sparkConf.set("spark.driver.host", System.getenv("IP"));
+    	sparkConf.set("spark.driver.bindAddress", "0.0.0.0");
+		sparkConf.set("spark.driver.port", "5050");
 
-    	sparkConf.setMaster("local[*]");
-    	sparkConf.set("spark.driver.host", "localhost");
 
-    	
     	JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
-    	
-  
-    	
-    	
+
     	JavaStreamingContext ssc = new JavaStreamingContext(sparkContext, new Duration(2000));
-    	
+
     	KafkaWeatherConsumer kfc = new KafkaWeatherConsumer();
-    	
+
     	kfc.consume(ssc);
-    	
-    	
-    	
+
+
     	sparkContext.close();
 
-    	
+
     }
 }
