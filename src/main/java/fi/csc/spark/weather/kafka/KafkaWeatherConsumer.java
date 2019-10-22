496b491bb6783fff16b63fc6fcbb8e7b7ced52da
juhahu <juha.hulkkonen@gmail.com>
2019-10-22 17:59:54 +0300

    First working version

--- src/main/java/fi/csc/spark/weather/kafka/KafkaWeatherConsumer.java
+++ src/main/java/fi/csc/spark/weather/kafka/KafkaWeatherConsumer.java
@@ -27,7 +27,7 @@ public class KafkaWeatherConsumer {
     public void consume(JavaStreamingContext streamingContext) throws InterruptedException {
 
         Map<String, Object> kafkaParams = new HashMap<String, Object>();
-        kafkaParams.put("bootstrap.servers", "localhost:9092");
+        kafkaParams.put("bootstrap.servers", "my-cluster-kafka-bootstrap:9092");
         kafkaParams.put("key.deserializer", LongDeserializer.class);
         kafkaParams.put("value.deserializer", StringDeserializer.class);
         kafkaParams.put("group.id", "weather00");
